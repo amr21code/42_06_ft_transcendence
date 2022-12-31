3f1558e122d7a29aaf0ec2e43fe59f3b5ee7572d
raweber <raweber@student.42wolfsburg.de>
2022-12-31 09:47:26 +0100

    for andi

--- frontend/src/components/MatchCourt.vue
+++ frontend/src/components/MatchCourt.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="match-court">
 		Match Court
-		
+		<div class="player_one"></div>
+		<div class="player_two"></div>
+		<div class="play-ball"></div>
 	</div>	
 </template>
 
@@ -28,5 +30,24 @@ export default defineComponent({
 			/* radial-gradient(circle at 50% 50%, white 0 14.75%, transparent 15%), */
 			linear-gradient(to right, transparent 49.75%, white 0 50.25%, transparent 0);
 		/* padding: 30px; */
+	}
+	.player_one {
+		/* background-color: blueviolet; */
+		height: 100px; /* MAKE ADJUSTABLE AND RESPONSIVE */
+		border-left: 10px solid blueviolet;
+		margin: 10px;
+	}
+	.player_two {
+		/* background-color: blueviolet; */
+		height: 100px; /* MAKE ADJUSTABLE AND RESPONSIVE */
+		border-right: 10px solid blueviolet;
+		margin: 10px;
+	}
+	.play-ball {
+		background-color: yellow;
+		height: 25px;
+		width: 25px;
+		border-radius: 50%;
+		margin-left: 100px;
 	}
 </style>
