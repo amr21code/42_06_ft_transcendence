3eb60b6e370ba9e943cbf5f42b863ac6a8a223eb
anruland <anruland@student.42wolfsburg.de>
2023-01-03 09:00:46 +0100

    REST API Usertest successfull

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,14 +11,21 @@ async function bootstrap() {
 	  		whitelist: true,
 		})
 	);
-		app.use(session({
-			secret: "390qofjsliufmpc90a3wrpoa938wmrcpaw3098rmcpa0",
-			saveUninitialized: false,
-			resave: false,
-			cookie: {
-				maxAge: 60000,
-			}
-		}))
+	app.enableCors({
+		origin: [
+			'http://localhost:5173',
+			'http://192.168.56.2:5173',
+		],
+		methods: ["GET", "POST"],
+	})
+	app.use(session({
+		secret: "390qofjsliufmpc90a3wrpoa938wmrcpaw3098rmcpa0",
+		saveUninitialized: false,
+		resave: false,
+		cookie: {
+			maxAge: 60000,
+		}
+	}))
 	app.use(passport.initialize());
 	app.use(passport.session());
   	await app.listen(3000);
