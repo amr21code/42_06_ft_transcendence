9f0a4967683bc241eab7f75657cb0f58848e354c
djedasch <djedasch@student.42wolfsburg.de>
2023-01-13 14:24:33 +0100

    2fa,compiling works, not tested

---
+++ backend/src/auth/twoFactorAuth.controller.ts
@@ -0,0 +1,61 @@
+import {  ClassSerializerInterceptor, Controller, Header, Post, UseInterceptors, Res, UseGuards, Req, HttpCode, Body, UnauthorizedException} from '@nestjs/common';
+import { TwoFactorAuthenticationService } from './twoFactorAuth.service';
+import { Response } from 'express';
+import { Request } from 'express';
+import { FtAuthGuard } from './guards/guards';
+import { UserService } from 'src/user/user.service';
+ 
+@Controller('2fa')
+@UseInterceptors(ClassSerializerInterceptor)
+export class TwoFactorAuthenticationController {
+  constructor(
+    private readonly twoFactorAuthService: TwoFactorAuthenticationService,
+	private readonly userService: UserService
+  ) {}
+ 
+  @Post('generate')
+  @UseGuards(FtAuthGuard)
+  async register(@Res() response: Response, @Req() request: Request) {
+    const { otpauthUrl } = await this.twoFactorAuthService.generateTwoFactorAuthenticationSecret(request);
+ 
+    return this.twoFactorAuthService.pipeQrCodeStream(response, otpauthUrl);
+  }
+
+  @Post('turn-on')
+  @HttpCode(200)
+  @UseGuards(FtAuthGuard)
+  async turnOnTwoFactorAuthentication(
+    @Req() request: Request,
+    @Body() { twoFactorAuthenticationCode }
+  ) {
+    const isCodeValid = this.twoFactorAuthService.isTwoFactorAuthenticationCodeValid(
+      twoFactorAuthenticationCode, request.user
+    );
+    if (!isCodeValid) {
+      throw new UnauthorizedException('Wrong authentication code');
+    }
+	const user = await this.userService.getMe(request.user);
+	await this.userService.changeUserData(user[0].userid, "2fa", 1);
+  }
+
+@Post('authenticate')
+  @HttpCode(200)
+  @UseGuards(FtAuthGuard)
+  async authenticate(
+    @Req() request: Request,
+    @Body() { twoFactorAuthenticationCode }
+  ) {
+    const isCodeValid = this.twoFactorAuthService.isTwoFactorAuthenticationCodeValid(
+      twoFactorAuthenticationCode, request.user
+    );
+    if (!isCodeValid) {
+      throw new UnauthorizedException('Wrong authentication code');
+    }
+ 
+    //const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(request.user.id, true);
+ 
+    //request.res.setHeader('Set-Cookie', [accessTokenCookie]);
+ 
+    return request.user;
+  }
+}
---
+++ backend/src/auth/twoFactorAuth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, Req } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { authenticator } from 'otplib';
+import { UserService } from '../user/user.service';
+import { Request } from 'express';
+import { toFileStream } from 'qrcode';
+ 
+@Injectable()
+export class TwoFactorAuthenticationService {
+  constructor (
+    private readonly userService: UserService,
+    private readonly configService: ConfigService
+  ) {}
+ 
+  public async generateTwoFactorAuthenticationSecret(@Req() request: Request) {
+    const secret = authenticator.generateSecret();
+	const user = await this.userService.getMe(request.user);
+    const otpauthUrl = authenticator.keyuri(user[0].userid, this.configService.get('TWO_FACTOR_AUTHENTICATION_APP_NAME'), secret);
+    await this.userService.changeUserData(user[0].userid, "2fa_secret", secret);
+    return {
+    	secret,
+    	otpauthUrl
+    }
+  }
+
+	public isTwoFactorAuthenticationCodeValid(twoFactorAuthenticationCode: string, user: any) {
+		return authenticator.verify({
+			token: twoFactorAuthenticationCode,
+			secret: user.twoFactorAuthenticationSecret
+	})
+	}
+
+   public async pipeQrCodeStream(stream: any, otpauthUrl: string) {
+    return toFileStream(stream, otpauthUrl);
+  }
+}
