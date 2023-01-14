6937cb5e9f635be124f0a63c15ee6ae71ecdf451
raweber <raweber@student.42wolfsburg.de>
2023-01-14 17:15:03 +0100

    small design fixes frontend

--- frontend/src/components/MatchCourt.vue
+++ frontend/src/components/MatchCourt.vue
@@ -272,17 +272,17 @@ export default defineComponent({
 		border: white solid;
 		color: red;
 		text-align: center;
-		aspect-ratio: 5/3;
 		border-radius: 4px;
 		background-image:
-			radial-gradient(circle at 50% 50%, transparent 15%, white 0 16%, transparent 16%),
-			radial-gradient(circle at 50% 50%, transparent 15%, white , transparent 500%),
-			radial-gradient(circle at 50% 50%, white 0 14.75%, transparent 15%),
-			linear-gradient(to right, transparent 49.75%, white 0 50.25%, transparent 0);
+		radial-gradient(circle at 50% 50%, transparent 15%, white 0 16%, transparent 16%),
+		radial-gradient(circle at 50% 50%, transparent 15%, white , transparent 500%),
+		radial-gradient(circle at 50% 50%, white 0 14.75%, transparent 15%),
+		linear-gradient(to right, transparent 49.75%, white 0 50.25%, transparent 0);
 		padding: 30px; */
-	/* } */
+		/* } */
 	#match-court {
 		width: 100%;
+		aspect-ratio: 5/3;
 		background-color: green;
 	}
 
