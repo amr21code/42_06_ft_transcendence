317c680955e8e30c9a90dc102a59d4b71ffd23b7
anruland <anruland@student.42wolfsburg.de>
2023-01-20 19:11:12 +0100

    backend: added debug output for online status

--- backend/src/user/user.gateway.ts
+++ backend/src/user/user.gateway.ts
@@ -10,30 +10,31 @@ import { UserService } from './user.service';
 export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	constructor (private readonly userService: UserService) {}
 
-	@SubscribeMessage('message')
-	handleConnection(client: any) {
+	@SubscribeMessage('user')
+	async handleConnection(client: any) {
 		// console.log(client.request.user);
 		if (client.request.user) {
 			// console.log("online");
 			const user = client.request.user;
-			// console.log("handle online", user);
 			if (user) {
-				this.userService.changeUserData(user.userid, "user_status", 1);
-				this.userService.changeUserData(user.userid, "socket_token", client.id);
+				await this.userService.changeUserData(user.userid, "user_status", 1);
+				await this.userService.changeUserData(user.userid, "socket_token", client.id);
 			}
+			console.log("handle online", await this.userService.getUserData(user.userid, "socket_token"), user.userid);
 		}
 	}
 	
-	handleDisconnect(client: any) {
+	async handleDisconnect(client: any) {
 		// console.log(client.request.user);
 		if (client.request.user) {
 			// console.log("offline");
 			const user = client.request.user;
-			// console.log("handle offline", user);
+			// console.log("handle offline", user, user.userid);
 			if (user) {
-				this.userService.changeUserData(user.userid, "user_status", 0);
-				this.userService.changeUserData(user.userid, "socket_token", "");
+				await this.userService.changeUserData(user.userid, "user_status", 0);
+				await this.userService.changeUserData(user.userid, "socket_token", "");
 			}
+			console.log("handle offline", await this.userService.getUserData(user.userid, "socket_token"), user.userid);
 		}
 	}
 
