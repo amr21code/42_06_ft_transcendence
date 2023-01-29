c989e1b5a79d93d262605f05a62fb8e8e3c98889
raweber <raweber@student.42wolfsburg.de>
2023-01-29 17:54:40 +0100

    canvas now should be sharp as a swiss army knife

--- frontend/src/components/MatchCourt.vue
+++ frontend/src/components/MatchCourt.vue
@@ -47,6 +47,7 @@ export default defineComponent({
 			if (!gameState) {
 				return;
 			}
+
 			console.log("game starts");
 			canvas.style.display = 'block';
 			matchWaitPopup.style.display = 'none';
@@ -159,7 +160,24 @@ export default defineComponent({
 
 
 		// ########### PAINTING ###################################################################################################
-        const paintGame = (state: any) => {
+        const resizeCanvasToDisplaySize = (canvas: any) => {
+			// Lookup the size the browser is displaying the canvas in CSS pixels.
+			const displayWidth  = canvas.clientWidth;
+			const displayHeight = canvas.clientHeight;
+			
+			// Check if the canvas is not the same size.
+			const needResize = canvas.width  !== displayWidth ||
+								canvas.height !== displayHeight;
+			
+			if (needResize) {
+				// Make the canvas the same size
+				canvas.width  = displayWidth;
+				canvas.height = displayHeight;
+			}
+		}
+		
+		const paintGame = (state: any) => {
+			resizeCanvasToDisplaySize(canvas);
             ctx.fillStyle = "#000";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
 
