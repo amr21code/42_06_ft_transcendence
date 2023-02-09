b1d11284d93eb75083fb4bacf8b991318c6dd419
artur01-code <yolojorit@gmail.com>
2023-02-09 08:54:46 +0100

    close pwdpopup

--- frontend/src/components/chat/pwdPopup.vue
+++ frontend/src/components/chat/pwdPopup.vue
@@ -68,6 +68,7 @@ export default defineComponent({
 			await DataService.createChat(String(id), password, 'join')
             .then((response: ResponseData) => {
                 SocketioService.refreshChats();
+				this.togglePwdPopup();
             })
             .catch((e: Error) => {
                 console.log(e);
